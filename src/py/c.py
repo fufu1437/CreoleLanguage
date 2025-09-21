92c67f9d2835ee03b651005002daa3592440b3d9
fufu1437 <128881350+fufu1437@users.noreply.github.com>
2025-09-21 16:06:28 +0800

    fufu1437

--- src/py/c.py
+++ src/py/c.py
@@ -2,31 +2,49 @@ import re
 # import sys
 f = open("main/creole.crl","r")
 
-# num = 0
-# for i in sys.argv:
-#     num+=1
-#     if i=="-f":
+class TokenType:
+    STRING = "STRING"
+    INTEGER = "INTEGER"
+    FLOAT = "FLOAT"
+    LPAREN = "LPAREN"
+    RPAREN = "RPAREN"
+    PLUS = "PLUS"
+    MINUS = "MINUS"
+    MULTIPLY = "MULTIPLY"
+    DIVIDE = "DIVIDE"
+    EOF = "EOF"
+    LCurly = "LCurly"  # {
+    RCurly = "RCurly"  # }
 
+class Token:
+    def __init__(self, text, value = None) -> None:
+        self.text = text
+        self.value = value
 
-# class Token_state():#state->状态
-#     def __init__(self) -> None:
-#         self.StringState  = None #StringState  -> 字符串状态
-#         self.IntegerState = None #IntegerState -> 整数状态
-#         self.SyntaxState  = None #SyntaxState  -> 语法状态
-#         self.FloatState   = None #FloatState   -> 浮点数状态
+    def __str__(self) -> str:
+        return f"Token({self.text}, {self.value})"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
 
+class Lexer:
+    def __init__(self,text) -> None:
+        self.Token = Token
+        self.Tokens = [
+            (re.search(r"\s+",text), None),
+            (re.search(r"\{",text), TokenType.LCurly),
+            (re.search(r"\}",text), TokenType.RCurly),
+            (re.search(r"\(",text), TokenType.LPAREN),
+            (re.search(r"\)",text), TokenType.RPAREN),
+            (re.search(r"\+",text), TokenType.PLUS),
+            (re.search(r"-",text), TokenType.MINUS),
+            (re.search(r"\*",text), TokenType.MULTIPLY),
+            (re.search(r"/",text), TokenType.DIVIDE),
+            (re.search(r"\d+\.\d+",text), TokenType.FLOAT),
+            (re.search(r"\d+",text), TokenType.INTEGER),
+            (re.search(r"[a-zA-Z_][a-zA-Z0-9_]*",text), TokenType.STRING)
+            ]
 
-def token(expression):
-    token_pattern = r"\bprint\b"
-    token = re.findall(token_pattern, expression)
-    return token
-
-
-
-str1 = "print('Hello, World!')"
-
-tokens = token(str1)
-
-print(tokens)
-
+a = Lexer("print da").Tokens
+print(a)
 f.close()
